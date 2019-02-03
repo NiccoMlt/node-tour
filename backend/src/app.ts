9bceea7bb954eee478099891bef127eaf176023b
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-02-03 22:21:08 +0100

    Fix code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,16 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+import express from "express"
+import path from "path"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
 
-var indexRouter = require('./routes')
-var usersRouter = require('./routes/users')
+const indexRouter = require('./routes')
+const usersRouter = require('./routes/users')
 
-var app = express()
+const app = express()
 
-app.use(logger('dev'))
+app.use(logger("dev"))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
